0da4f220adfa05bcfc1fdcfde08d1d39113c75ba
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-08-25 14:17:38 -0700

    Added Bar Chart | need to convert otu_labels into str

--- app.js
+++ app.js
@@ -7,21 +7,37 @@ d3.json("samples.json").then(function(data){
     // Confirming the data was passed succesfully
     console.log(data);
 
+// Bar Chart
+
     // Define an array to store samples
-    var samples_array = Array();
+    var values_array = Array();
 
     // An array for the otu_ids
-    var id_array = Array();
+    var otu_id_array = Array();
 
     // An array for the otu_labels
+    var otu_labels_array = Array();
 
-    // Iterate over each element in the samples object
+    // Iterate over each element in the json object to append arrays
     data.samples.forEach(element => {
-        samples_array.push(element.sample_values);
-        id_array.push(element.otu_ids);
+        values_array.push(element.sample_values);
+        otu_id_array.push(element.otu_ids);
+        otu_labels_array.push(element.otu_labels);
     });
 
-    console.log(id_array.slice(0 , 2));
+    // Rendering the initial bar chart
+        var bar_data = [{
+            x: values_array[0].slice(0,10),
+            labels: otu_id_array[0].slice(0,10),
+            orientation: 'h' ,
+            type: 'bar'
+
+        }];
+
+        Plotly.newPlot("bar" , bar_data)
+
+
+    console.log( values_array[0][0]);
 
     
 
