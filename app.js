bba1919ff0829d55d5f97b9d554e6c16b89dab91
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-08-25 17:49:24 -0700

    Converted all of the otu ids into strings:

--- app.js
+++ app.js
@@ -7,37 +7,52 @@ d3.json("samples.json").then(function(data){
     // Confirming the data was passed succesfully
     console.log(data);
 
-// Bar Chart
+// Static Bar Chart
+    
+    // Creating an Array to hold sample_values , otu_ids , otu_labels
+    // Contains a list of sample arrays , withth first element being a header
+    var data_array = [['values'],['ids'], ['labels']]
 
-    // Define an array to store samples
-    var values_array = Array();
-
-    // An array for the otu_ids
-    var otu_id_array = Array();
-
-    // An array for the otu_labels
-    var otu_labels_array = Array();
-
-    // Iterate over each element in the json object to append arrays
+    // Iterating over the samples in the data and storing the top 10 otu data into arrays
     data.samples.forEach(element => {
-        values_array.push(element.sample_values);
-        otu_id_array.push(element.otu_ids);
-        otu_labels_array.push(element.otu_labels);
+
+        data_array[0].push(element.sample_values.slice(0,10));
+
+        // Converting the otu labels into string
+        var id_array = []; // stores the otu_id strings
+        for (i = 0; i < element.otu_ids.slice(0,10).length; i++){
+            
+            id_array.push(element.otu_ids.slice(0,10)[i].toString());
+        };
+
+        data_array[1].push(id_array);
+
+        data_array[2].push(element.otu_labels.slice(0,10))
+
+    
+        console.log(data_array);
+
+
     });
 
-    // Rendering the initial bar chart
-        var bar_data = [{
-            x: values_array[0].slice(0,10),
-            labels: otu_id_array[0].slice(0,10),
-            orientation: 'h' ,
-            type: 'bar'
+    //for (var i = 0; i < data_array[1][1].length; i++){
+      //  data_array[1][1][i] = data_array[1][1][i].toString();
+    //}
 
-        }];
+    //test_arr[0].push([1,2,3,4,5,6,7,8,9,10]);
 
-        Plotly.newPlot("bar" , bar_data)
+    //for( var i = 0; i < test_arr[0][1].length; i++){
+    //    test_arr[0][1][i] = test_arr[0][1][i].toString()
+    //}
 
 
-    console.log( values_array[0][0]);
+
+
+
+
+  
+
+    
 
     
 
